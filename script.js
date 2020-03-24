ea8f2f116f559cc6b717008dba27274f158b3b70
asedlack <asedlackcode@gmail.com>
2020-03-23 19:59:54 -0700

    added sidenav

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $('.sidenav').sidenav();
     $('.modal').modal();
     jQuery.ajaxPrefilter(function(options) {
         if (options.crossDomain && jQuery.support.cors) {
