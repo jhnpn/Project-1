bd1c81cd356940a28cc97ea77befba5ef5217384
umaiibo <j.hnpn123@gmail.com>
2020-03-23 18:55:31 -0700

    created a javascript file for webpage

---
+++ assets/JavaScript/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    $('.modal').modal();
+
+    var searchTrigger = function(){
+        // var term = 'mario'
+        $.ajax({
+            url: 'https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games/?search=&fields=name,id',
+            type: 'post',
+            headers: {
+                'Accept':'application/json',
+                'user-key':'6fc2ca57683cbf5b7414f3a79d44161a',
+                'Access-Control-Allow-Origin':'localhost:5500'
+            
+            }   
+        }).then(function(response){
+            console.log('this is my response ' + response)
+            return response
+        }).then(function(data){
+            console.log(data);
+        })
+        .catch(function(error){
+            console.log('This is my error '+ error)
+        });
+    }
+
+    searchTrigger();
+
+    $('#search').on('keypress', function(e){
+        let term = e.target.value
+        console.log(e.key);
+        if (e.key === "Enter") {
+            e.preventDefault();
+            $.ajax({
+                url: 'https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games/?search='+term+'&fields=name,id',
+                type: 'post',
+                headers: {
+                    'Accept':'application/json',
+                    'user-key':'6fc2ca57683cbf5b7414f3a79d44161a',
+                    'Access-Control-Allow-Origin':'localhost:5500'
+                
+                }   
+            }).then(function(response){
+                console.log('this is my response ' + response)
+                return response
+            }).then(function(data){
+                console.log(data);
+            })
+            .catch(function(error){
+                console.log('This is my error '+ error)
+            });
+        }
+    
+        }
+        
+    )
+});
